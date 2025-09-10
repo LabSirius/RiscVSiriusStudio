1f82e058c735b728d2527158f424a3e40a516843
amirhurtado <a.hurtado@utp.edu.co>
2025-09-10 15:42:35 -0500

    Fixed bug in initialization and available table

--- examples/assemble/sum_n 2.asm
+++ examples/assemble/sum_n 2.asm
@@ -1,15 +1,62 @@
-# sum_n.asm
-# Computes the sum of the first n numbers
-# input: a0 = n
-# output: t0 = sum(1..n)
-    li a0, 5        # n = 5
-    li t0, 0        # sum = 0
-    li t1, 1        # i = 1
-loop:
-    bgt t1, a0, end # if i > n, break
-    add t0, t0, t1  # sum += i
-    addi t1, t1, 1  # i++
-    beq zero,zero, loop  # jump to loop
-end:
-    ebreak
-    # result is in t0
+# This example shows an implementation of the mathematical
+# factorial function (! function) to find the factorial value of !7 = 5040.
+
+.data
+argument: .word   7
+str1:     .string "Factorial value of "
+str2:     .string " is "
+
+.text
+main:
+        lw  a0, argument   # Load argument from static data
+        jal ra, fact       # Jump-and-link to the 'fact' label
+
+        # Print the result to console
+        mv  a1, a0
+        lw  a0, argument
+        jal ra, printResult
+
+        # Exit program
+        li a7, 10
+        ecall
+
+fact:
+        addi sp, sp, -16
+        sw   ra, 8(sp)
+        sw   a0, 0(sp)
+        addi t0, a0, -1
+        bge  t0, zero, nfact
+
+        addi a0, zero, 1
+        addi sp, sp, 16
+        jr x1
+
+nfact:
+        addi a0, a0, -1
+        jal  ra, fact
+        addi t1, a0, 0
+        lw   a0, 0(sp)
+        lw   ra, 8(sp)
+        addi sp, sp, 16
+        mul a0, a0, t1
+        ret
+
+# --- printResult ---
+# a0: Value which factorial number was computed from
+# a1: Factorial result
+printResult:
+        mv t0, a0
+        mv t1, a1
+        la a0, str1
+        li a7, 4
+        ecall
+        mv a0, t0
+        li a7, 1
+        ecall
+        la a0, str2
+        li a7, 4
+        ecall
+        mv a0, t1
+        li a7, 1
+        ecall
+        ret
