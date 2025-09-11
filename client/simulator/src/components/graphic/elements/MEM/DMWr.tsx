e398d8ce7c0ccf7fd8bf992768699eaa7257673b
Amir Hurtado <a.hurtado@utp.edu.co>
2025-09-11 14:52:21 -0500

    Implementation of connections in most control signals

--- client/simulator/src/components/graphic/elements/MEM/DMWr.tsx
+++ client/simulator/src/components/graphic/elements/MEM/DMWr.tsx
@@ -7,14 +7,18 @@ import { useSimulator } from '@/context/shared/SimulatorContext';
 
 export default function DMWR() {
   const { overlayMemoryActive} = useOverlay();
-  const { currentType} = useCurrentInst();
-  const { isFirstStep } = useSimulator();
+  const { currentType, pipelineValuesStages} = useCurrentInst();
+  const { isFirstStep, typeSimulator } = useSimulator();
+
+
+  const isActive = typeSimulator === "pipeline" ?    pipelineValuesStages.MEM.instruction.type === "S" || (pipelineValuesStages.MEM.instruction.type === "I" && pipelineValuesStages.MEM.instruction.opcode === "0000011") : currentType === "L" || currentType === "S"
+
 
   return (
     <div className='w-full'>
 
        <div className='relative w-full h-full'>
-       <h2 className={` titleInElement top-[-1.9rem]  left-[50%] -translate-x-[50%]  ${overlayMemoryActive && 'overlay-scale'} ${!(currentType === "L" || currentType === "S" || !isFirstStep) && '!text-[#D3D3D3]'  }`}>DMWR</h2>
+       <h2 className={` titleInElement top-[-1.9rem]  left-[50%] -translate-x-[50%]  ${overlayMemoryActive && 'overlay-scale'} ${!(isActive || !isFirstStep) && '!text-[#D3D3D3]'  }`}>DMWR</h2>
        </div>
 
        <div  className={`${overlayMemoryActive && 'overlay-moveX-t'}`} >
