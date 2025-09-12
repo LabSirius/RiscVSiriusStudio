25f0b9997b95897f19df66978af0e2e0f5bec1fa
Amir Hurtado <a.hurtado@utp.edu.co>
2025-09-11 22:47:56 -0500

    Logic for disabling and enabling control signals in a completed pipeline

--- client/simulator/src/components/graphic/elements/ID/ControlUnit/outputTunnels/MemoryTunnel.tsx
+++ client/simulator/src/components/graphic/elements/ID/ControlUnit/outputTunnels/MemoryTunnel.tsx
@@ -1,19 +1,16 @@
-import { useCurrentInst } from "@/context/graphic/CurrentInstContext";
 import TunnelContainer from "./TunnelContainer"
 import { useOverlay } from "@/context/graphic/OverlayContext";
-import { useSimulator } from "@/context/shared/SimulatorContext";
 
 
 const MemoryTunnel = () => {
   const { setOverlayMemoryActive } = useOverlay();
-  const { currentType } = useCurrentInst();
-  const { isFirstStep} = useSimulator();
+
 
   return (
     <div className="relative w-full ml-[17rem] rotate-270"
     onMouseEnter={() => setOverlayMemoryActive(true)}
     onMouseLeave={() => setOverlayMemoryActive(false)}>
-    <h2 className={`subtitleInTunnel ${(currentType === "L" || currentType === "S" || !isFirstStep) ? '!text-[#000000]' : '!text-[#D3D3D3]'  }`}>
+    <h2 className={`subtitleInTunnel !text-[#000000]  }`}>
        Memory
       </h2>
         <TunnelContainer color={'#E8F5E9'} />
