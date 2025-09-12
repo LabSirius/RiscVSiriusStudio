1823c484bfd83c5908e4bb1988d79dd0c06002ea
Amir Hurtado <a.hurtado@utp.edu.co>
2025-09-11 22:47:56 -0500

    Logic for disabling and enabling control signals in a completed pipeline

--- client/simulator/src/components/graphic/elements/WB/RUDataWrSrc.tsx
+++ client/simulator/src/components/graphic/elements/WB/RUDataWrSrc.tsx
@@ -6,31 +6,34 @@ import { useSimulator } from "@/context/shared/SimulatorContext";
 
 export default function RUDataWrSrc() {
   const { overlayWBActive } = useOverlay();
-  const { typeSimulator} = useSimulator()
+  const { typeSimulator, isEbreak, operation } = useSimulator();
   const { currentType, pipelineValuesStages } = useCurrentInst();
 
-  const inactive = typeSimulator === "pipeline" ?  pipelineValuesStages.WB.instruction.type === "S" ||  pipelineValuesStages.WB.instruction.type === "B"  || pipelineValuesStages.WB.instruction.pc === -1 : (currentType === "S" || currentType === "B")
+  const inactive =
+    typeSimulator === "pipeline"
+      ? pipelineValuesStages.WB.instruction.type === "S" ||
+        pipelineValuesStages.WB.instruction.type === "B" ||
+        pipelineValuesStages.WB.instruction.pc === -1
+      : currentType === "S" || currentType === "B";
   return (
     <div className="w-full">
       <div className="relative w-full h-full">
         <h2
           className={` titleInElement top-[.3rem] left-[50%] -translate-[50%]   ${
             overlayWBActive && "overlay-scale"
-          } ${inactive && "!text-[#D3D3D3]"}`}>
+          } ${(operation !== "uploadMemory" ? inactive || isEbreak : false) &&  "!text-[#D3D3D3]"}`}>
           RUDataWrSrc
         </h2>
       </div>
 
-      
-        <div className={`${overlayWBActive && "overlay-moveX"}`}>
-          <Handle
-            type="source"
-            position={Position.Top}
-            className="output-tunnel"
-            style={{ top: "-.4rem" }}
-          />
-        </div>
-
+      <div className={`${overlayWBActive && "overlay-moveX"}`}>
+        <Handle
+          type="source"
+          position={Position.Top}
+          className="output-tunnel"
+          style={{ top: "-.4rem" }}
+        />
+      </div>
     </div>
   );
 }
