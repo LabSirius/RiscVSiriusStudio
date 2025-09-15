844c686b146debd19db086c9775fec9d5e8ad1e2
Amir Hurtado <a.hurtado@utp.edu.co>
2025-09-15 12:51:59 -0500

    Correction in title of instruction decode in graphic

--- client/simulator/src/components/graphic/Canva/shared/nodes/ID.ts
+++ client/simulator/src/components/graphic/Canva/shared/nodes/ID.ts
@@ -25,7 +25,7 @@ export const useIDNodes = (): Node[] => {
       },
     },
 
-    nodeBase("title-ID", "title", "Decode (ID)", { x: 10, y: 0 }, "ID", isPipeline ? 800 :870, 50),
+    nodeBase("title-ID", "title", "Decode (ID)", { x: 10, y: 0 }, "ID", isPipeline ? 800 : 700, 50),
 
     nodeBase("instIDPipeline", "instIDPipeline", "instIDPipeline", { x: 65, y: 10 }, "ID", 730, 50),
 
