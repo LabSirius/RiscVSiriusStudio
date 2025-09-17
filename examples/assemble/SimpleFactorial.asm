94279cfa0f9e5adce0d84e70528383a7d6eecc43
Amir Hurtado <a.hurtado@utp.edu.co>
2025-09-17 11:51:38 -0500

    Correction in examples because the memory logic was changed and they previously worked with a memory size x

--- examples/assemble/SimpleFactorial.asm
+++ examples/assemble/SimpleFactorial.asm
@@ -1,25 +1,18 @@
-li a0, 3  # factorial(3) 
-fact:
-  # arg: n in a0, returns n! in a0
-  addi sp, sp, -8     # reserve stack space for 2 registers
-  sw   ra, 0(sp)      # save the return address on the stack
+# factorial.asm
+# Computes the factorial of n
+# input: a0 = n
+# output: t0 = n!
 
-  li   t0, 2
-  blt  a0, t0, ret_one # if n < 2 (i.e., 0 or 1), result is 1
+    li a0, 5        # n = 5 (you can change this value)
+    li t0, 1        # result = 1
+    li t1, 1        # i = 1
 
-  sw   a0, 4(sp)      # save our current n on the stack
-  addi a0, a0, -1     # n = n - 1
-  jal  fact           # recursive call: fact(n-1)
-                      # result of fact(n-1) is now in a0
+loop:
+    bgt t1, a0, end   # if i > n, exit loop
+    mul t0, t0, t1    # result *= i
+    addi t1, t1, 1    # i++
+    beq zero, zero, loop  # unconditional jump to loop
 
-  lw   t0, 4(sp)      # restore original n from stack into t0
-  mul  a0, t0, a0     # a0 = n * fact(n-1)
-  j    done
-
-ret_one:
-  li   a0, 1          # base case: result is 1
-
-done:
-  lw   ra, 0(sp)      # restore return address from stack
-  addi sp, sp, 8      # free our stack frame
-  jr   ra             # and return
+end:
+    ebreak
+    # final result is stored in t0
